da92d4acc5078b5e9d36bd62e61b82592793325a
Lisa Ihde <lisaihde@web.de>
2019-12-01 06:28:57 +0100

    Update files

--- message.ino
+++ message.ino
@@ -1,80 +1,66 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <EEPROM.h>
-
-#include <Wire.h> 
-#include <SSD1306.h> // oled display
-#include "credentials.h"
 #include <Servo.h>
+#include "SSD1306Wire.h"
 
-int pos = 90;
-int increment = -1;
-String line;
-String modus;
-char idSaved;
-bool Read;
-
-Servo myservo;  // servo object
-SSD1306  display(0x3c, D2, D1);
-
-// network credentials
+#include "credentials.h"
 const char* ssid = _ssid;
 const char* password = _password;
+const String url = _url;
 
-void initDisplay() {
-  display.init();
-
-  display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10); 
-}
+SSD1306Wire oled(0x3C, D2, D1);
+Servo myservo; 
+int pos = 90;
+int increment = -1;
+int lightValue;
+String line;
+String modus;
+char idSaved; 
+bool wasRead;  
 
 void drawMessage(const String& message) {
-  display.clear();
+  oled.clear();
 
-  // differentiate between text and image
+  // Unterscheide zwischen Text und Bild
   if(modus[0] == 't'){
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.setFont(ArialMT_Plain_10);
-    display.drawStringMaxWidth(0, 0, 128, message);
+    oled.drawStringMaxWidth(0, 0, 128, message);    
   } 
   else {
-    display.setColor(WHITE);
     for(int i = 0; i <= message.length(); i++){
       int x = i % 129;
       int y = i / 129;
     
       if(message[i] == '1'){
-        display.setPixel(x, y);
+        oled.setPixel(x, y);
       }
     } 
-  } 
-     
-  display.display();
+  }    
+  oled.display();
 }
 
 void wifiConnect() {
-  // connect to wifi
   if (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, password);
   
-    // wait for connection
+    // Warte auf Verbindung
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
     }
   }
 }
 
-void getMessage() {
+void getGistMessage() {
   const int httpsPort = 443;
   const char* host = "gist.githubusercontent.com";
+  const char fingerprint[] = "CC AA 48 48 66 46 0E 91 53 2C 9C 7C 23 2A B1 74 4D 29 9D 33";
   
   WiFiClientSecure client;
+  client.setFingerprint(fingerprint);
   if (!client.connect(host, httpsPort)) {
-    return; // connection failed
+    return; // Verbindung fehlgeschlagen
   }
   
-  const String url = _url;
-
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "User-Agent: ESP8266\r\n" +
@@ -83,62 +69,65 @@ void getMessage() {
   while (client.connected()) {
     String temp = client.readStringUntil('\n');
     if (temp == "\r") {
-      break; // headers received
+      break;
     }
   }
   String id = client.readStringUntil('\n'); 
-  if(id[0] != idSaved){
-    Read = 0;
+  if(id[0] != idSaved){ // Neue Nachricht
+    wasRead = 0;
     idSaved = id[0];
     EEPROM.write(142, idSaved);
-    EEPROM.write(144, Read);
-    EEPROM.commit(); // new message received
+    EEPROM.write(144, wasRead);
+    EEPROM.commit(); 
+
+    modus = client.readStringUntil('\n');
+    line = client.readStringUntil(0);
+    drawMessage(line);
   }
-  modus = client.readStringUntil('\n');
-  line = client.readStringUntil(0);
 }
 
 void spinServo(){
-    myservo.attach(16);       // attaches D0 to the servo object
-    myservo.write(pos);       // servo goes to actual 'pos'
-    delay(50);                // waits 50ms to reach the position
+    myservo.write(pos);      
+    delay(50);    // Warte 50ms um den Servo zu drehen
 
-    if(pos == 75 || pos == 105){
+    if(pos == 75 || pos == 105){ // Drehbereich zwischen 75°-105°
       increment *= -1;
     }
     pos += increment;
 }
 
 void setup() {
-  Serial.begin(9600);
-
-  initDisplay();
-
-  display.clear();
-  display.drawString(30, 30, "<3 LOVEBOX <3");
-  display.display();
+  myservo.attach(16);       // Servo an D0
   
-  drawMessage("Verbindung zum Internet wird aufgebaut..");
+  oled.init();
+  oled.flipScreenVertically();
+  oled.setColor(WHITE);
+  oled.setTextAlignment(TEXT_ALIGN_LEFT);
+  oled.setFont(ArialMT_Plain_10);
+     
+  oled.clear();
+  oled.drawString(30, 30, "<3 LOVEBOX <3");
+  oled.display();
+  
   wifiConnect();
 
   EEPROM.begin(512);
   idSaved = EEPROM.get(142, idSaved);
-  Read = EEPROM.get(144, Read);
+  wasRead = EEPROM.get(144, wasRead);
 }
 
 void loop() {
-  getMessage(); 
-  drawMessage(line);
+  if(wasRead){
+    getGistMessage();   
+  }
   
-  int lightValue = analogRead(0);
-  while(!Read){   
-    spinServo();
-    lightValue = analogRead(0);      // update sensor
-    if(lightValue < 800) { 
-      Read = 1;
-      EEPROM.write(144, Read);
+  while(!wasRead){   
+    spinServo();    // Drehe Herz
+    lightValue = analogRead(0);      // Lese Helligkeitswert
+    if(lightValue > 300) { 
+      wasRead = 1;
+      EEPROM.write(144, wasRead);
       EEPROM.commit();
     }
   }
 }
-
